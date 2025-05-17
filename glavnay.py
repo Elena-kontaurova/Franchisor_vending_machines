6fa906ac06f56f2147737f540434e69c651c2cae
Elena-kontaurova <kontaurova0306@gmail.com>
2025-05-17 15:00:13 +0300

    yes

--- glavnay.py
+++ glavnay.py
@@ -2128,6 +2128,7 @@ def open_polsov(_):
 
 
 def open_modem(_):
+    """123"""
     ff = tk.Label(root, background='#060a0d', width=100, height=10)
     ff.place(x=800, y=50)
     gg = tk.Label(root, text='Администирование / Модемы',
